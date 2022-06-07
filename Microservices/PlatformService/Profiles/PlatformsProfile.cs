2b0538d3a9b40395bb678545f8100e3383f041c8
Piotr Kącki <piotrekacki@gmail.com>
2022-06-07 16:59:09 +0200

    Add Dtos and Mapping

---
+++ Microservices/PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using PlatformService.Dtos;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformsProfile : Profile
+    { 
+        public PlatformsProfile()
+        {
+            //Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+
+
+
+    }
+}
